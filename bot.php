00d3f9afcc3ebe41cb57b3e561f78dc7f1c39d2d
neovison <44082677+neovison@users.noreply.github.com>
2018-10-12 17:31:16 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -5,7 +5,7 @@
     $mess = $data->object->body;
 
     $user_info = json_decode(file_get_contents($conf['apiurl'].'users.get?user_id='.$u_id.'&v='.$conf['v'].'&access_token='.$conf['standalone']));
-    $user_name = $user_info->$response[0]->$first_name;
+    $user_name = $user_info->response[0]->first_name;
 
     $temp_link = file ($conf['temp_link']);
     if ($temp_link[0] != $album) {
