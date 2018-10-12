725b0822ad78eb0c227659c0b2fb0a9bc85db788
neovison <44082677+neovison@users.noreply.github.com>
2018-10-12 18:46:32 +0700

    Update test.php

--- test.php
+++ test.php
@@ -8,7 +8,7 @@
     $arr = explode('_', $path);
     $owner_id = $arr[0];
     $album_id = $arr[1];
-    $standalone = "e1133f86f00fed298885a32d2a3b372a232ff54ee2ff825d3463f3862fc78541f8a070f7cefe4a4700b4";
+    $standalone = 'e1133f86f00fed298885a32d2a3b372a232ff54ee2ff825d3463f3862fc78541f8a070f7cefe4a4700b4';
     $group_token = '5cb91e923115ea48e6d809cbbd866559000cb17342cf530369aa4230c0dfe14934fab41b3edea02290674';
     $conf = [
     	'standalone' => $standalone,
